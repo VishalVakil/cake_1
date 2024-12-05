d32550e3ddcb48ffcc817d4411f0e0435824fa7e
VishalVakil <vakil.vishal17@gmail.com>
2024-12-04 21:53:12 -0700

    Added files

---
+++ next.config.js
@@ -0,0 +1,7 @@
+module.exports = {
+    // Enable static export
+    output: 'export',
+    // If you're using a custom path (e.g., `/my-next-site/`), set the basePath
+    basePath: '/Cake_1',  // replace with your GitHub repo name
+    assetPrefix: '/Cake_1/',  // replace with your GitHub repo name
+};
